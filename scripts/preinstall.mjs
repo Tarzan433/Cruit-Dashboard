211f88cda72bc2bbd8880d23bf2bad1c62f00664
Tarzan433 <t8857352@gmail.com>
2026-07-22 00:55:16 +0530

    Updated preinstall.mjs

--- scripts/preinstall.mjs
+++ scripts/preinstall.mjs
@@ -2,8 +2,7 @@ import { unlinkSync } from "node:fs";
 
 const userAgent = process.env.npm_config_user_agent ?? "";
 if (!userAgent.includes("pnpm")) {
-  console.error("Use pnpm instead");
-  process.exit(1);
+  console.warn("Use pnpm instead");
 }
 
 for (const file of ["package-lock.json", "yarn.lock"]) {
